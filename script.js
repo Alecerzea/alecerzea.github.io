46e58fcbbe8b8701a1e62db1793e870ea2a34ff2
alecerzea <miguelcervanteszea@hotmail.com>
2023-12-11 20:15:30 -0500

    main

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
    var certificados = [
+    { nombre: "Cerificado de B2 de MCervantes.pdf", descripcion: "certificado de B2 obtenido en el año 2020" },
     { nombre: "Miguel AlejandroCervantes Zea-P60 - FUNDAMENTO-certificate.pdf", descripcion: "certificado de Linux essentials obtenido en el año 2022" },
     { nombre: "Miguel AlejandroCervantes Zea-CCNA RS ESSENTIA-certificate.pdf", descripcion: "certificado de Switching, Routing and Wireless Essentials obtenido en el año 2023" },
     { nombre: "Python_Essentials_1_Badge20230730-28-kwpti5.pdf", descripcion: "certificado de Python Essentials 1 obtenido en el año 2023" },
     { nombre: "Python_Essentials_2_Badge20230731-28-ghjx4s.pdf", descripcion: "certificado de Python Essentials 2 obtenido en el año 2023" },
-    { nombre: "JavaScript_Essentials_1_Badge20230807-28-41bn52.pdf", descripcion: "certificado de JavaScript Essentials obtenido en el año 2023" },
-    { nombre: "Cerificado de B2 de MCervantes.pdf", descripcion: "certificado de B2 obtenido en el año 2020" }
+    { nombre: "JavaScript_Essentials_1_Badge20230807-28-41bn52.pdf", descripcion: "certificado de JavaScript Essentials obtenido en el año 2023" }
 ];
 
 
