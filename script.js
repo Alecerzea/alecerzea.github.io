12721fc563464f5c340ce21c37f7981a8fcb77be
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-14 19:37:43 -0500

    main

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-   var certificados = [
-    { nombre: "Miguel AlejandroCervantes Zea-P60 - FUNDAMENTO-certificate.pdf", descripcion: "certificado de Linux essentials obtenido en el año 2022" },
-    { nombre: "Miguel AlejandroCervantes Zea-CCNA RS ESSENTIA-certificate.pdf", descripcion: "certificado de Switching, Routing and Wireless Essentials obtenido en el año 2023" },
-    { nombre: "Python_Essentials_1_Badge20230730-28-kwpti5.pdf", descripcion: "certificado de Python Essentials 1 obtenido en el año 2023" },
-    { nombre: "Python_Essentials_2_Badge20230731-28-ghjx4s.pdf", descripcion: "certificado de Python Essentials 2 obtenido en el año 2023" },
-    { nombre: "JavaScript_Essentials_1_Badge20230807-28-41bn52.pdf", descripcion: "certificado de JavaScript Essentials obtenido en el año 2023" },
-    { nombre: "JavaScript_Essentials_2_Badge20240615-7-23egq", descripcion: "certificado de JavaScript Essentials 2 obtenido en el año 2024" }
-];
-
-
+    var certificados = [
+        { nombre: "Miguel AlejandroCervantes Zea-P60 - FUNDAMENTO-certificate.pdf", descripcion: "certificado de Linux essentials obtenido en el año 2022" },
+        { nombre: "Miguel AlejandroCervantes Zea-CCNA RS ESSENTIA-certificate.pdf", descripcion: "certificado de Switching, Routing and Wireless Essentials obtenido en el año 2023" },
+        { nombre: "Python_Essentials_1_Badge20230730-28-kwpti5.pdf", descripcion: "certificado de Python Essentials 1 obtenido en el año 2023" },
+        { nombre: "Python_Essentials_2_Badge20230731-28-ghjx4s.pdf", descripcion: "certificado de Python Essentials 2 obtenido en el año 2023" },
+        { nombre: "JavaScript_Essentials_1_Badge20230807-28-41bn52.pdf", descripcion: "certificado de JavaScript Essentials obtenido en el año 2023" },
+        { nombre: "JavaScript_Essentials_2_Badge20240615-7-23egq", descripcion: "certificado de JavaScript Essentials 2 obtenido en el año 2024" }
+    ];
+    
+    
     function mostrarCertificados() {
     var divCurriculum = document.getElementById('Curriculum de Miguel Alejandro Cervantes Zea');
 
